d7e3e24ae8e426acd86064e2800f7297a815d069
Meghedi Hacopi <hacopian.m@gmail.com>
2020-03-02 23:53:49 -0800

    Have the cases working

--- liri.js
+++ liri.js
@@ -1,36 +1,43 @@
-//nrequire("dotenv").config();
-var keys = require("./keys.js");
+require("dotenv").config();
+
+var keys = require("./keys");
 var axios = require("axios");
 var moment = require('moment');
+var fs = require("fs");
 
-//var spotify = new Spotify(keys.spotify);
+var Spotify = require('node-spotify-api');
+
+var spotify = new Spotify(keys.spotify);
 
 let command = process.argv[2];
-let thirdArg = process.argv.slice(3).join("+");
+let userInput = process.argv.slice(3).join("+");
 
+main(command);
 
-switch (command) {
-    case "concert-this":
-        bandsInTown();
-        break;
-    case "spotify-this-song":
-        spotify();
-        break;
-    case "movie-this":
-        omdb();
-        break;
-    case "do-what-it-says":
-        dowhatItSays();
-        break;
+function main(command) {
+    switch (command) {
+        case "concert-this":
+            bandsInTown();
+            break;
+        case "spotify-this-song":
+            spotifySearch();
+            break;
+        case "movie-this":
+            omdb();
+            break;
+        case "do-what-it-says":
+            doWhatItSays();
+            break;
+    }
 }
 
-function bandsInTown(){
+function bandsInTown() {
 
-    axios.get("https://rest.bandsintown.com/artists/"+ thirdArg +"/events?app_id=codingbootcamp").then(function(response){
+    axios.get("https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp").then(function (response) {
         let txtHtml = '';
-        for(let i=0; i<response.data.length; i++){
-           txtHtml += '\n===================================================================\n';
-           txtHtml += `Name of the Venue: ${response.data[i].venue.name}
+        for (let i = 0; i < response.data.length; i++) {
+            txtHtml += '\n===================================================================\n';
+            txtHtml += `Name of the Venue: ${response.data[i].venue.name}
            Venue Location: ${response.data[i].venue.city} ${response.data[i].venue.country}
            Date of the Event: ${moment(response.data[i].datetime).format("MM/DD/YY")}`;
         }
@@ -41,12 +48,15 @@ function bandsInTown(){
 }
 
 
-function omdb(){
-    url = "http://www.omdbapi.com/?apikey=trilogy&t="+ thirdArg ;
+function omdb() {
+    if(!userInput){
+       userInput = 'Mr Nobody';
+    }
+    url = "http://www.omdbapi.com/?apikey=trilogy&t=" + userInput;
     let txtHtml = '';
-    axios.get(url).then(function(response){
+    axios.get(url).then(function (response) {
         let res = response.data;
-         txtHtml += `
+        txtHtml += `
          * Title of the movie : ${res.Title}
          * Year the movie came out : ${res.Year}
          * IMDB Rating of the movie: ${res.imdbRating}
@@ -55,10 +65,48 @@ function omdb(){
          * Language of the movie: ${res.Language}
          * Plot of the movie: ${res.Plot}
          * Actors in the movie: ${res.Actors}`;
-         
-         console.log(txtHtml);
+
+        console.log(txtHtml);
     });
 }
+
+
+function spotifySearch(){
+    spotify.search({ type: 'track', query: userInput }, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+        let txtHtml='';
+        for(let i=0; i<3; i++){
+            txtHtml += `
+            ===========================================
+            Artists : ${data.tracks.items[i].artists[0].name}
+            Song Name: ${data.tracks.items[i].name}
+            Preview Link: ${data.tracks.items[i].external_urls.spotify}
+            Album Name: ${data.tracks.items[i].album.name}
+            `;
+        }
+       
+      console.log(txtHtml); 
+      });
+}
+
+
+function doWhatItSays() {
+    fs.readFile('random.txt', 'utf8', function (err, data) {
+        if(err){
+            console.log(err);
+        }
+        let firstCommand = data.split(",")[0];
+        let secondCommand = data.split(",")[1];
+         command = firstCommand;
+         userInput = secondCommand;
+
+         main(command);
+        });
+}
+
+
 
 
 
